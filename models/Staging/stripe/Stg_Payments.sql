344c6589da7251f144d676bd04c1abdd3bf43015
Julius Kuijer <juliuskuijer@hotmail.com>
2026-06-17 16:08:19 +0000

    1

--- models/Staging/stripe/Stg_Payments.sql
+++ models/Staging/stripe/Stg_Payments.sql
@@ -6,8 +6,8 @@ select
     status,
 
     -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount') }} as amount,
     
-    amount/100 as amount,
     created as created_at
 
 from {{source('stripe','payment')}}
